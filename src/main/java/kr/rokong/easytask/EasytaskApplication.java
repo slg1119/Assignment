afe5499bdcc84c959e7d4364b4f55330d01b3042
RokHyung Son <fhrgud1264@gmail.com>
2024-10-07 14:05:34 +0900

    first commit

---
+++ src/main/java/kr/rokong/easytask/EasytaskApplication.java
@@ -0,0 +1,13 @@
+package kr.rokong.easytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasytaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/rokong/easytask/EasytaskApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.rokong.easytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasytaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
